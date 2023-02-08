ad1d6ece8bf9dec5e23cc81722554678dbfbfd0b
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2023-02-08 16:14:43 -0300

    Setting app home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "articles#index"
   get "/articles", to: "articles#index" # The route above declares that GET /articles requests are mapped to the index action of ArticlesController.
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
