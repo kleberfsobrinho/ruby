208a089a3b5fc9972306865187f2d04b5d56aab6
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2023-02-09 12:06:39 -0300

    add: showing a single article

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "articles#index"
   get "/articles", to: "articles#index" # The route above declares that GET /articles requests are mapped to the index action of ArticlesController.
+  get "/articles/:id", to: "articles#show"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
