15498e2e10673c6c64fa7422fda741b58bfa561f
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2023-02-09 14:16:34 -0300

    add: route for Comment

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # get "/articles", to: "articles#index" # The route above declares that GET /articles requests are mapped to the index action of ArticlesController.
   # get "/articles/:id", to: "articles#show"
   
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
